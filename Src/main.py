2e53198f395901a4c42e9f73466d416e8fa512fb
monkim <monkim803@gmail.com>
2026-07-19 16:52:00 +0900

    Update main.py

--- Src/main.py
+++ Src/main.py
@@ -10,6 +10,10 @@ from OrderManager import *
 from RiskManager import *
 from Strategy import *
 
+#市場がクローズする時間
+Const MARKET_CLOSE_TIME = 16
+#17:00-24:00まで、情報収集ストップ時間（7時間）
+Const COLLECT_STOP_TIME = 3600 * 7
 
 def load_config(path="config.yaml"):
     with open(path, "r", encoding="utf-8") as f:
@@ -42,24 +46,32 @@ def main():
     # --- メインループ ---
     while True:
         try:
+            previous = now
             now = datetime.now()
-            print(f"[{now}] Checking market conditions...")
-
-            market_data = api.get_market_data()
-            signal = strategy.generate_signal(market_data)
-
-            if signal is not None:
-                print(f"Signal detected: {signal}")
-                trader.execute(signal)
-
-            time.sleep(interval)
-
+            #日付が変わった場合
+            if previous.Day != now.Day:
+                #日次バッチ処理を実行したかどうか
+                daily_done = False
+            elif daily_done = False:
+                daily_batch_procedure()
+                daily_done = True
+            #マーケットクローズ後（念のため17時以降）
+            elif now.hour > MARKET_CLOSE_TIME:
+                #7時間スリープ
+                time.sleep(COLLECT_STOP_TIME)
+            else:
+                print(f"[{now}] Checking market conditions...")
+                market_data = api.get_market_data()
+                signal = strategy.generate_signal(market_data)
+                if signal is not None:
+                    print(f"Signal detected: {signal}")
+                    trader.execute(signal)
+                time.sleep(interval)
         except Exception as e:
-            print("Error occurred:", e)
+            print("例外が発生しました:", e)
             traceback.print_exc()
-            print("Retrying in 10 seconds...")
+            print("10秒後にリトライします...")
             time.sleep(10)
-
 
 if __name__ == "__main__":
     main()
